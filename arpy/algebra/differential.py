1f3155fd7c5c4fd0d55ef5bb0c794a6ad485d7da
sminez <innes.morrison@cocoon.life>
2017-02-19 19:48:49 +0000

    adding ability to specify metric and division type in differentials

--- arpy/algebra/differential.py
+++ arpy/algebra/differential.py
@@ -6,12 +6,12 @@ In Cartesian coordinates this is:
     Dμ = ∂μ/αμ = ∂ / (αμ∂xμ) = α0∂0 - αi∂i = α0∂0 - ∇i
 '''
 from copy import deepcopy
-from .config import ALLOWED, DIVISION_TYPE
+from .config import ALLOWED, DIVISION_TYPE, METRIC
 from .ar_types import Alpha, MultiVector
 from .operations import div_by, div_into
 
 
-def component_partial(component, wrt, div):
+def component_partial(component, wrt, div, metric):
     '''
     Symbolically differentiate a component by storing the partials and
     converting the alpha value using the correct division type.
@@ -20,9 +20,9 @@ def component_partial(component, wrt, div):
     #        component get copied as well.
     new_component = deepcopy(component)
     if div == 'by':
-        new_component.alpha = div_by(new_component.alpha, wrt)
+        new_component.alpha = div_by(new_component.alpha, wrt, metric)
     elif div == 'into':
-        new_component.alpha = div_into(new_component.alpha, wrt)
+        new_component.alpha = div_into(new_component.alpha, wrt, metric)
     else:
         raise ValueError('Invalid division specification: %s' % div)
     # new_component.xi.add_partial(wrt)
@@ -31,7 +31,7 @@ def component_partial(component, wrt, div):
     return new_component
 
 
-def AR_differential(mvec, wrt, div=DIVISION_TYPE):
+def AR_differential(mvec, wrt, div=DIVISION_TYPE, metric=METRIC):
     '''
     Compute the result of Differentiating a each component of a MultiVector
     with respect to a given list of unit elements under the algebra.
@@ -39,22 +39,23 @@ def AR_differential(mvec, wrt, div=DIVISION_TYPE):
     comps = []
     for component in mvec:
         for element in wrt:
-            comps.append(component_partial(component, Alpha(element), div))
+            comp = component_partial(component, Alpha(element), div, metric)
+            comps.append(comp)
     return MultiVector(comps)
 
 
 def differential_operator(wrt):
     '''Define a new operator as a function for later use'''
-    def operator(mvec):
-        return AR_differential(mvec, wrt)
+    def operator(mvec, div=DIVISION_TYPE, metric=METRIC):
+        return AR_differential(mvec, wrt, div, metric)
     return operator
 
 
-def Dmu(mvec):
+def Dmu(mvec, div=DIVISION_TYPE, metric=METRIC):
     '''The main operator from the paper'''
-    return AR_differential(mvec, ['0', '1', '2', '3'])
+    return AR_differential(mvec, ['0', '1', '2', '3'], div, metric)
 
 
-def DG(mvec):
+def DG(mvec, div=DIVISION_TYPE, metric=METRIC):
     '''A full derivative with respect to all components'''
-    return AR_differential(mvec, ALLOWED)
+    return AR_differential(mvec, ALLOWED, div, metric)
