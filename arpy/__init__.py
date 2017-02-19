f8219c2aa4e781d70092c4b780f1e7545c8c51f5
sminez <innes.morrison@cocoon.life>
2017-02-19 19:49:36 +0000

    adding 4-set differentials as defaults

--- arpy/__init__.py
+++ arpy/__init__.py
@@ -9,15 +9,22 @@ from .utils.lexparse import ARContext
 ##############################################################################
 # Multi-vectors to work with based on the 4-vector components
 XiG = MultiVector([Pair(a) for a in ALLOWED])
-XiM = MultiVector([Pair('p')] + [Pair(a) for a in XI_GROUPS['jk']])
+XiB = MultiVector([Pair('p')] + [Pair(a) for a in XI_GROUPS['jk']])
 XiT = MultiVector([Pair('0')] + [Pair(a) for a in XI_GROUPS['0jk']])
 XiA = MultiVector([Pair('123')] + [Pair(a) for a in XI_GROUPS['i']])
 XiE = MultiVector([Pair('0123')] + [Pair(a) for a in XI_GROUPS['i0']])
+
 # Multi-vectors based on grade
 Xi1 = project(XiG, 1)
 Xi2 = project(XiG, 2)
 Xi3 = project(XiG, 3)
 Xi4 = project(XiG, 4)
+
+# 4-set Differentials
+DB = differential_operator(XI_GROUPS['jk'])
+DT = differential_operator(XI_GROUPS['0jk'])
+DA = differential_operator(XI_GROUPS['i'])
+DE = differential_operator(XI_GROUPS['i0'])
 
 # Build the default context for computation
 # NOTE:: The user can create a new context in the same way or modify the
