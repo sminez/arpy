78ab6d2821a9bc0ce6b30e2bb3645d77a508597b
sminez <innes.morrison@cocoon.life>
2017-02-20 11:56:48 +0000

    adding cayley visualisation functon

--- arpy/utils/visualisation.py
+++ arpy/utils/visualisation.py
@@ -1,23 +1,21 @@
 '''
 Tools for visualising results from calculations and investigations.
-http://seaborn.pydata.org/generated/seaborn.heatmap.html
 '''
-from ..config import ALLOWED
-from .algebra import a
+from ..algebra.config import ALLOWED
+from ..algebra.ar_types import Alpha
+from ..algebra.operations import full
 
 
-def print_cayley(op="full"):
+def cayley(op=full, padding=6):
     '''
-    Dump the current Cayley table to the terminal allowing for specification
+    Print current Cayley table to the terminal allowing for specification
     of the operation used to compute the table.
-    '''
-    if op == "full":
-        cayley = [[a(i) * a(j) for j in ALLOWED] for i in ALLOWED]
-    elif op == "into":
-        cayley = [[a(i).inverse() * a(j) for j in ALLOWED] for i in ALLOWED]
-    elif op == "by":
-        cayley = [[a(i) * a(j).inverse() for j in ALLOWED] for i in ALLOWED]
 
-    for k in cayley:
-        line = [str(a).rjust(6) for a in k]
-        print(" ".join(line))
+    Any function that accepts two Alphas can be passed as op.
+    '''
+    comps = (
+        ' '.join([str(op(Alpha(a), Alpha(b))).rjust(padding) for a in ALLOWED])
+        for b in ALLOWED
+    )
+    for comp in comps:
+        print(comp)
