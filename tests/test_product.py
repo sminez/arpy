303db0ef665c0d7d6238e2b8e00540545159145a
sminez <innes.morrison@cocoon.life>
2017-02-27 11:26:17 +0000

    moving to paramatized tests

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,57 +1,54 @@
-from itertools import combinations
+import pytest
+from utils import ap, neg_ap, imetric, ijmetric, ijkmetric
 from arpy import Alpha, find_prod, ALLOWED
 
 
-ap = Alpha('p')
-neg_ap = Alpha('p', -1)
-txyz = ['0', '1', '2', '3']
-
-
-def test_alpha_p():
+@pytest.mark.parametrize('index,metric', imetric)
+def test_alpha_p(index, metric):
     '''
     Left and right multiplication by αp is idempotent.
     Left and right multiplication by -αp negates.
     '''
-    for index in ALLOWED:
-        test_alpha = Alpha(index)
-        assert find_prod(ap, test_alpha) == test_alpha
-        assert find_prod(test_alpha, ap) == test_alpha
+    test_alpha = Alpha(index)
+    assert find_prod(ap, test_alpha, metric=metric) == test_alpha
+    assert find_prod(test_alpha, ap, metric=metric) == test_alpha
 
-        negative_test_alpha = Alpha(index, -1)
-        assert find_prod(neg_ap, test_alpha) == negative_test_alpha
-        assert find_prod(test_alpha, neg_ap) == negative_test_alpha
+    negative_test_alpha = Alpha(index, -1)
+    assert find_prod(neg_ap, test_alpha, metric=metric) == negative_test_alpha
+    assert find_prod(test_alpha, neg_ap, metric=metric) == negative_test_alpha
 
 
-def test_swapped_indices():
+@pytest.mark.parametrize('i,j,metric', ijmetric)
+def test_swapped_indices_ij(i, j, metric):
     '''
     Swapping two indices negates the product
     '''
-    two_cases = [c for c in combinations(txyz, 2)]
-    three_cases = [c for c in combinations(txyz, 3)]
-
-    for case in two_cases:
-        i, j = Alpha(case[0]), Alpha(case[1])
-        ij, ji = find_prod(i, j), find_prod(j, i)
-        assert ij.index == ji.index
-        assert ij.sign == ji.sign * -1
-
-    for case in three_cases:
-        i, j, k = Alpha(case[0]), Alpha(case[1]), Alpha(case[2])
-        ijk = find_prod(find_prod(i, j), k)
-        jik = find_prod(find_prod(j, i), k)
-        assert ijk.index == jik.index
-        assert ijk.sign == jik.sign * -1
+    i, j = Alpha(i), Alpha(j)
+    ij, ji = find_prod(i, j, metric=metric), find_prod(j, i, metric=metric)
+    assert ij.index == ji.index
+    assert ij.sign == ji.sign * -1
 
 
-def test_squaring():
+@pytest.mark.parametrize('i,j,k,metric', ijkmetric)
+def test_swapped_indices_ijk(i, j, k, metric):
+    '''
+    Swapping two indices negates the product
+    '''
+    i, j, k = Alpha(i), Alpha(j), Alpha(k)
+    ijk = find_prod(find_prod(i, j, metric=metric), k, metric=metric)
+    jik = find_prod(find_prod(j, i, metric=metric), k, metric=metric)
+    assert ijk.index == jik.index
+    assert ijk.sign == jik.sign * -1
+
+
+@pytest.mark.parametrize('index,metric', imetric)
+def test_squaring(index, metric):
     '''
     The product of an element with itself is +-αp
     '''
-    for index in ALLOWED:
-        pos = Alpha(index)
-        neg = Alpha(index, -1)
+    pos, neg = Alpha(index), Alpha(index, -1)
 
-        assert find_prod(pos, pos) in [ap, neg_ap]
-        assert find_prod(neg, neg) in [ap, neg_ap]
-        assert find_prod(pos, neg) in [ap, neg_ap]
-        assert find_prod(pos, neg) in [ap, neg_ap]
+    assert find_prod(pos, pos, metric) in [ap, neg_ap]
+    assert find_prod(neg, neg, metric) in [ap, neg_ap]
+    assert find_prod(pos, neg, metric) in [ap, neg_ap]
+    assert find_prod(pos, neg, metric) in [ap, neg_ap]
