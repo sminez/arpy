30019dbb7c33fc1050e5644de99e174e3e3124a9
sminez <innes.morrison@cocoon.life>
2017-03-07 09:35:59 +0000

    stubing out tests for deggen

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,7 +1,7 @@
 import pytest
 from utils import metrics, ap, neg_ap
 from arpy import Alpha, Pair, MultiVector, find_prod, inverse, dagger, \
-    commutator, full, ALLOWED
+    commutator, project, ALLOWED
 
 
 @pytest.mark.parametrize('metric', metrics)
@@ -12,7 +12,7 @@ def test_inverse(metric):
     for index in ALLOWED:
         alpha = Alpha(index)
         inverse_alpha = inverse(alpha, metric=metric)
-        assert find_prod(alpha, inverse_alpha, metric=metric) == Alpha('p')
+        assert find_prod(alpha, inverse_alpha, metric=metric) == ap
 
 
 @pytest.mark.parametrize('metric', metrics)
@@ -34,3 +34,31 @@ def test_commutator(metric):
         for j in ALLOWED:
             aj = Alpha(j)
             assert commutator(ai, aj, metric=metric) in [ap, neg_ap]
+
+
+def test_project_alpha():
+    '''
+    Projecting an Alpha should return the value if and only if the
+    number of indices on the Alpha is the grade we are projecting
+    '''
+    # NOTE:: (deggen) The project function is in arpy/algebra/operations.py
+    pass
+
+
+def test_project_pair():
+    '''
+    Projecting a Pair should return the value if and only if the
+    number of indices on the Pair's Alpha is the grade we are projecting
+    '''
+    pass
+
+
+def test_project_multivector():
+    '''
+    Projecting a MultiVector should return a new MultiVector that contains
+    only pairs that are of the correct grade
+    '''
+    # NOTE:: (Deggen) The `new MultiVector` part is important! ;)
+    #        Have a look at test_new_component_partial in the
+    #        test_differentials file.
+    pass
