c4aa6a793f4e2f132c056a0da7f8ed6f0b3810a0
sminez <innes.morrison@cocoon.life>
2017-06-21 14:21:24 +0100

    adding in defining multivectors to calculation files

--- arpy/__main__.py
+++ arpy/__main__.py
@@ -7,6 +7,7 @@ The file is fed through and ARContext with some aditional pre-parsing in
 order to deal with assignment and paramater setting. All Variables are printed
 when at the head of the output and calculations are labelled.
 '''
+import re
 import argparse
 from collections import namedtuple
 from .utils.lexparse import ARContext
@@ -30,6 +31,9 @@ form of a *.arp calculation file. The format of an arp file is as follows:
 // ALLOWED: p 23 31 12 0 023 031 012 123 1 2 3 0123 01 02 03
 // METRIC: +---
 
+# Define a multivector
+odd = {1, 2, 3, 023, 031, 012}
+
 # Compute FDF
 DF = Dmu ^ F
 FDF = F ^ DF
@@ -51,12 +55,16 @@ class CalculationError(Exception):
     pass
 
 
-comment = namedtuple('comment', 'lnum text')
+raw = namedtuple('raw', 'lnum text')
 step = namedtuple('step', 'lnum var args')
 context_update = namedtuple('context_update', 'lnum param val')
+mvec_def = namedtuple('mvec_def', 'lnum var alphas')
+
+mvec_pattern = r'([a-zA-Z_][a-zA-Z_0-9]*)\s?=\s?\{([p0213, ]*)\}$'
 
 
-def parse_calculation_file(fname):
+def parse_calculation_file(fname, default_allowed=config.allowed,
+                           default_metric=config.metric):
     '''Configure the ARContext for carrying out the calculation'''
     def convert_metric(s):
         s = s[:-1]  # remove trailing newline
@@ -73,28 +81,28 @@ def parse_calculation_file(fname):
     with open(fname, 'r') as f:
         for lnum, line in enumerate(f):
             lnum += 1
-            # Skip blank lines
+
             if line == '\n':
-                continue
+                lines.append(raw(lnum, ''))
 
             # Check and set paramaters
             elif line.startswith('// METRIC:'):
                 m = convert_metric(line.split('// METRIC: ')[1])
                 if metric is None:
                     metric = m
-                lines.append(comment(lnum, line.strip()))
+                lines.append(raw(lnum, line.strip()))
                 lines.append(context_update(lnum, 'metric', m))
 
             elif line.startswith('// ALLOWED:'):
                 a = line.split('// ALLOWED: ')[1].split()
                 if allowed is None:
                     allowed = a
-                lines.append(comment(lnum, line.strip()))
+                lines.append(raw(lnum, line.strip()))
                 lines.append(context_update(lnum, 'allowed', a))
 
             # extract comments
             elif line.startswith('#'):
-                lines.append(comment(lnum, line.strip()))
+                lines.append(raw(lnum, line.strip()))
 
             # extract steps
             else:
@@ -102,18 +110,26 @@ def parse_calculation_file(fname):
                     tmp = 'Steps must assign to a variable:\n[{}] {}'
                     raise CalculationError(tmp.format(lnum, line))
                 else:
-                    var, args = line.split(' = ')
-                    lines.append(step(lnum, var, args.strip()))
+                    # Check for multivector assignent
+                    mvec_match = re.match(mvec_pattern, line)
+                    if mvec_match:
+                        var, alphas = mvec_match.groups()
+                        alphas = re.split(', |,| ', alphas)
+                        lines.append(mvec_def(lnum, var, alphas))
+                    else:
+                        # Try to parse an ar command
+                        var, args = line.split(' = ')
+                        lines.append(step(lnum, var, args.strip()))
 
     # Fall back to defaults if metric/allowed were not specified
     if allowed is None:
-        allowed = config.allowed
-        lines = comment(0, '// ALLOWED: ' + ' '.join(allowed))
+        allowed = default_allowed
+        lines = raw(0, '// ALLOWED: ' + ' '.join(allowed))
 
     if metric is None:
-        metric = config.metric
+        metric = default_metric
         m = ''.join('+' if x == 1 else '-' for x in metric)
-        lines = comment(0, '// METRIC: ' + m)
+        lines = raw(0, '// METRIC: ' + m)
 
     config.allowed = allowed
     config.metric = metric
@@ -127,18 +143,18 @@ parser = argparse.ArgumentParser(
     formatter_class=argparse.RawDescriptionHelpFormatter
 )
 
-parser.add_argument(
-    '-v',
-    '--vector',
-    action='store_true',
-    help='display the result in vector calculus notation'
-)
-parser.add_argument(
-    '-s',
-    '--simplify',
-    action='store_true',
-    help='simplify results before printing'
-)
+# parser.add_argument(
+#     '-v',
+#     '--vector',
+#     action='store_true',
+#     help='display the result in vector calculus notation'
+# )
+# parser.add_argument(
+#     '-s',
+#     '--simplify',
+#     action='store_true',
+#     help='simplify results before printing'
+# )
 parser.add_argument(
     '-l',
     '--latex',
@@ -152,7 +168,7 @@ args = parser.parse_args()
 context, lines = parse_calculation_file(args.script)
 
 for l in lines:
-    if isinstance(l, comment):
+    if isinstance(l, raw):
         print(l.text)
 
     elif isinstance(l, context_update):
@@ -163,12 +179,14 @@ for l in lines:
         elif l.param == 'allowed':
             context.allowed = l.val
 
+    elif isinstance(l, mvec_def):
+        exec('{} = MultiVector({})'.format(l.var, l.alphas))
+        eval('''print('{} = ', {})'''.format(l.var, l.var))
+
     elif isinstance(l, step):
         exec('{} = context("{}")'.format(l.var, l.args))
         print('{} = {}'.format(l.var, l.args))
         if args.latex:
-            eval('''print('{} = ', {}.__tex__())'''.format(l.var, l.var))
+            eval('print({}.__tex__())'.format(l.var, l.var))
         else:
-            eval('''print('{} = ', {})'''.format(l.var, l.var))
-        # Split calculation steps with newlines
-        print('')
+            eval('print({})'.format(l.var, l.var))
