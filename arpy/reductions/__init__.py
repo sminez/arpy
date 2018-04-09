087b28aa47dc4681005675c05c0c1dfc26298598
sminez <innesdmorrison@gmail.com>
2018-04-09 14:02:18 +0100

    pulling reducer functions up into arpy.reductions

---
+++ arpy/reductions/__init__.py
@@ -0,0 +1,12 @@
+'''
+A utility module for performing term level simplification and notation
+changes on MultiVectors.
+'''
+from .reducers import SUB_SCRIPTS, SUPER_SCRIPTS, Template, \
+    Term, Replacement, chain_reducers
+
+
+__all__ = [
+    'SUB_SCRIPTS', 'SUPER_SCRIPTS', 'Template',
+    'Term', 'Replacement', 'chain_reducers'
+]
