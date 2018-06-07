25b238c843f46fea802c918f89ccc6a99ac1b1f7
sminez <innesdmorrison@gmail.com>
2018-06-07 15:31:05 +0100

    adding Zet and Nat utility function

--- arpy/utils/utils.py
+++ arpy/utils/utils.py
@@ -15,6 +15,36 @@ def Tex(obj):
     print(obj.__tex__())
 
 
+def Zet(alpha):
+    '''Return the Zet of a given alpha value.'''
+    # Conditions for being a member of each Zet
+    zet_map = {
+        # `e` elements (NOTE: `p` is a special case)
+        (1, True): 'T', (3, False): 'A', (4, True): 'E',
+        # `x, y, z` elements: (len, has '0')
+        (2, False): 'B', (3, True): 'T', (1, False): 'A', (2, True): 'E',
+    }
+    ix = alpha.index
+
+    if ix == 'p':
+        return 'B'
+    else:
+        return zet_map[(len(ix), 0 in ix)]
+
+
+def Nat(alpha):
+    '''Return the Nature of a given Alpha.'''
+    # Element sets for each e,x,y,z nature
+    nat_map = {
+        set('p'): 'e', set('123'): 'e', set('0'): 'e', set('0123'): 'e',
+        set('1'): 'x', set('23'): 'x', set('023'): 'x', set('01'): 'x',
+        set('2'): 'y', set('31'): 'y', set('031'): 'y', set('02'): 'y',
+        set('3'): 'z', set('12'): 'z', set('012'): 'z', set('03'): 'z',
+    }
+
+    return nat_map[set(alpha.index)]
+
+
 def reorder_allowed(allowed, order='pBtThAqE'):
     '''
     Shuffle the ordering of allowed, keeping 3-Vectors together.
