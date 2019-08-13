46795e3c15ce62233d994a10f0a1dc18eb68ba1a
Innes Anderson-Morrison <innes.andersonmorrison@anaplan.com>
2019-08-13 15:37:39 +0100

    simplifying some of the logic in find_prod

--- arpy/algebra/operations.py
+++ arpy/algebra/operations.py
@@ -1,4 +1,4 @@
-'''
+"""
 arpy (Absolute Relativity in Python)
 Copyright (C) 2016-2018 Innes D. Anderson-Morrison All rights reserved.
 
@@ -33,7 +33,7 @@ If the final position of the first element is even then it will take an odd
 number of pops to correctly position it. We can then look only at the remaining
 elements and re-label them with indices 1->(n-1) and repeat the process until
 we are done.
-'''
+"""
 from copy import deepcopy
 from functools import wraps
 from types import FunctionType
@@ -43,121 +43,92 @@ from ..utils.concepts.dispatch import dispatch_on
 from .ar_types import Alpha, Pair, XiProduct
 
 
+POINT = "p"
+
+
 def product_cache(func):
-    '''
-    A simple caching decorator that knows how to convert the args of
-    find_prod into a hashable for returning previous results.
-    NOTE: The functools lru_cache doesn't work as lists are not
-          hashable.
-    '''
     cache = dict()
-    cache_hits = {'hits': 0, 'misses': 0}
 
     @wraps(func)
     def wrapped(i, j, cfg=cfg):
-        '''
-        Check the cache for a result and call through to find_prod if
-        we don't have one.
-        '''
         args = (i, j, tuple(cfg.metric), tuple(cfg.allowed))
         result = cache.get(args)
 
         if result:
-            # Return the result from the cache
-            cache_hits['hits'] += 1
-            return result
-        else:
-            # Compute the result and store it in the cache
-            cache_hits['misses'] += 1
-            result = func(i, j, cfg=cfg)
-            cache[args] = result
             return result
 
-    # Make the cache and metrics available on the wrapped function
+        result = func(i, j, cfg=cfg)
+        cache[args] = result
+        return result
+
     wrapped.cache = cache
-    wrapped.cache_hits = cache_hits
     return wrapped
 
 
 @product_cache
 def find_prod(i, j, cfg=cfg):
-    '''
+    """
     Compute the product of two alpha values in the algebra. This uses some
     optimisations and observations that I've made in order to speed up the
     computation.
 
     NOTE: find_prod ALWAYS returns a new alpha as we don't want to mutate
           the values passed in as that will mess up any future calculations!
-    '''
-    # Confirm that the alphas passed in are valid for this config
+    """
     if not (i.allowed == j.allowed == cfg.allowed):
-        raise ValueError(
-            ('Inconsistant allowed values detected when computing a product.\n'
-             'Config allowed: {}\n'
-             'Passed values: "α{}" "α{}"').format(
-                 cfg.allowed, i.index, j.index)
-        )
+        err = "Inconsistant allowed values detected when computing a product.\n"
+        err += 'Config allowed: {}\nPassed values: "{}" "{}"'.format(cfg.allowed, i, j)
+        raise ValueError(err)
 
-    # NOTE:: These are here so that they the user can change metric and
-    #        allowed without having to reload the module.
-    metric = {k: v for k, v in zip('0123', cfg.metric)}
+    metric = {k: v for k, v in zip("0123", cfg.metric)}
     targets = {frozenset(a): a for a in cfg.allowed}
-
-    # Rule (1) :: Multiplication by αp is idempotent
-    if i.index == 'p':
-        return Alpha(j.index, (i.sign * j.sign), cfg=cfg)
-    elif j.index == 'p':
-        return Alpha(i.index, (i.sign * j.sign), cfg=cfg)
-
-    # Rule (2) :: Squaring and popping
     sign = i.sign * j.sign
     components = i.index + j.index
-    intersection = set(i.index).intersection(set(j.index))
 
-    for repeated in intersection:
+    # Multiplication by αp is idempotent
+    if POINT in components:
+        index = components.replace(POINT, "", 1)
+        return Alpha(index, sign, cfg=cfg)
+
+    # Pop and cancel matching components
+    for repeated in set(i.index).intersection(set(j.index)):
         first = components.find(repeated)
         second = components.find(repeated, first + 1)
         n_pops = second - first - 1
-        sign *= (-1 if (n_pops % 2 == 1) else 1)
+        sign *= -1 if (n_pops % 2 == 1) else 1
         sign *= metric[repeated]
-        components = ''.join(c for c in components if c != repeated)
+        components = "".join(c for c in components if c != repeated)
 
-    # If everything cancelled then i == j and we are left with αp (r-point)
     if len(components) == 0:
-        return Alpha('p', sign)
+        return Alpha(POINT, sign)
 
-    # Rule (3) :: Popping to the correct order
     target = targets[frozenset(components)]
 
     if target == components:
         return Alpha(target, sign, cfg=cfg)
 
-    ordering = {c: i+1 for i, c in enumerate(target)}
+    ordering = {c: i + 1 for i, c in enumerate(target)}
     current = [ordering[c] for c in components]
 
     while len(current) > 1:
         if current[0] % 2 == 0:
             sign *= -1
         current = current[1:]
-        new_order = {j: i+1 for i, j in enumerate(sorted(current))}
+        new_order = {j: i + 1 for i, j in enumerate(sorted(current))}
         current = [new_order[k] for k in current]
 
     return Alpha(target, sign, cfg=cfg)
 
 
 def inverse(a, cfg=cfg):
-    '''Find the inverse of an Alpha element'''
-    return Alpha(
-        a.index,
-        (find_prod(a, a, cfg).sign * a.sign),
-        cfg=cfg
-    )
+    """Find the inverse of an Alpha element"""
+    return Alpha(a.index, (find_prod(a, a, cfg).sign * a.sign), cfg=cfg)
 
 
 ##############################################################################
 @dispatch_on((0, 1))
 def full(a, b, cfg=cfg):
-    '''Compute the Full product of two elements'''
+    """Compute the Full product of two elements"""
     raise NotImplementedError
 
 
@@ -215,6 +186,7 @@ def _full_mvec_alpha(m, a, cfg=cfg):
     prod.replacements = m.replacements
     return prod
 
+
 # NOTE:: Definitions of the full product involving differnetials are found in
 #        differential.py due to import conflicts.
 
@@ -222,7 +194,7 @@ def _full_mvec_alpha(m, a, cfg=cfg):
 ##############################################################################
 @dispatch_on((0, 1))
 def div_by(a, b, cfg=cfg):
-    '''Divide one element by another'''
+    """Divide one element by another"""
     raise NotImplementedError
 
 
@@ -240,7 +212,7 @@ def _div_by_pair_alpha(a, b, cfg=cfg):
 ##############################################################################
 @dispatch_on((0, 1))
 def div_into(a, b, cfg=cfg):
-    '''Divide one element into another'''
+    """Divide one element into another"""
     raise NotImplementedError
 
 
@@ -258,17 +230,17 @@ def _div_into_Alpha_Pair(a, b, cfg=cfg):
 ##############################################################################
 @dispatch_on(index=0)
 def project(element, grade, cfg=cfg):
-    '''
+    """
     Implementation of the grade-projection operator <A>n.
     Return only the elements of A that are of grade n.
     NOTE:: αp is a grade-0 scalar element.
-    '''
+    """
     raise NotImplementedError
 
 
 @project.add(Alpha)
 def _project_alpha(element, grade, cfg=cfg):
-    if element.index == 'p':
+    if element.index == POINT:
         if grade == 0:
             return element
         else:
@@ -281,7 +253,7 @@ def _project_alpha(element, grade, cfg=cfg):
 
 @project.add(Pair)
 def _project_pair(element, grade, cfg=cfg):
-    if element.alpha.index == 'p':
+    if element.alpha.index == POINT:
         if grade == 0:
             return element
         else:
@@ -297,12 +269,12 @@ def _project_multivector(element, grade, cfg=cfg):
     correct_grade = []
     if grade == 0:
         for component in element:
-            if component.alpha.index == 'p':
+            if component.alpha.index == POINT:
                 correct_grade.append(component)
     else:
         for component in element:
             ix = component.alpha.index
-            if len(ix) == grade and ix != 'p':
+            if len(ix) == grade and ix != POINT:
                 correct_grade.append(component)
     res = MultiVector(correct_grade, cfg=cfg)
     res.replacements.extend(element.replacements)
@@ -311,44 +283,34 @@ def _project_multivector(element, grade, cfg=cfg):
 
 ##############################################################################
 
+
 @dispatch_on((0, 1, 2))
 def prod_apply(arg1, arg2, arg3=None, cfg=cfg):
-    '''
+    """
     Apply a function to the cartesian product of two multivectors
     NOTE:: The function must act on a single Multivector.
-    '''
+    """
     raise NotImplementedError
 
 
 @prod_apply.add((FunctionType, MultiVector, MultiVector))
 def _prod_apply_dmm(func, mv1, mv2, cfg=cfg):
     if not isinstance(mv1, MultiVector) and isinstance(mv2, MultiVector):
-        raise TypeError('Arguments must be a MultiVectors')
-    return MultiVector(
-        (full(i, j, cfg)
-         for i in func(mv1, cfg=cfg)
-         for j in mv2),
-        cfg=cfg
-    )
+        raise TypeError("Arguments must be a MultiVectors")
+    return MultiVector((full(i, j, cfg) for i in func(mv1, cfg=cfg) for j in mv2), cfg=cfg)
 
 
 @prod_apply.add((MultiVector, FunctionType, MultiVector))
 def _prod_apply_mdm(mv1, func, mv2, cfg=cfg):
     if not isinstance(mv1, MultiVector) and isinstance(mv2, MultiVector):
-        raise TypeError('Arguments must be a MultiVectors')
-    return MultiVector(
-        (full(i, j, cfg)
-         for i in mv1
-         for j in func(mv2, cfg=cfg)),
-        cfg=cfg
-    )
+        raise TypeError("Arguments must be a MultiVectors")
+    return MultiVector((full(i, j, cfg) for i in mv1 for j in func(mv2, cfg=cfg)), cfg=cfg)
 
 
 @prod_apply.add((FunctionType, tuple, type(None)))
 def _prod_apply_d_mm(func, mvecs=(None, None), _=None, cfg=cfg):
-    if not isinstance(mvecs[0], MultiVector) and \
-            isinstance(mvecs[1], MultiVector):
-        raise TypeError('Arguments must be a MultiVectors')
+    if not isinstance(mvecs[0], MultiVector) and isinstance(mvecs[1], MultiVector):
+        raise TypeError("Arguments must be a MultiVectors")
     return func(full(mvecs[0], mvecs[1], cfg))
 
 
@@ -357,16 +319,17 @@ def _prod_apply_d_mm(func, mvecs=(None, None), _=None, cfg=cfg):
 
 @dispatch_on(index=0)
 def dagger(obj, cfg=cfg):
-    '''
+    """
     Compute the Hermitian conjugate of the argument.
-    '''
+    """
     raise NotImplementedError
 
 
 @dagger.add(MultiVector)
 def _dagger_mvec(mvec, cfg=cfg):
     _neg = [
-        Alpha(a, cfg=cfg) for a in cfg.allowed
+        Alpha(a, cfg=cfg)
+        for a in cfg.allowed
         if full(Alpha(a, cfg=cfg), Alpha(a, cfg=cfg), cfg).sign == -1
     ]
     mvec = deepcopy(mvec)
@@ -400,11 +363,12 @@ def _dagger_pair(pair, cfg=cfg):
 
 ##############################################################################
 
+
 @dispatch_on((0, 1))
 def commutator(a, b, cfg=cfg):
-    '''
+    """
     Computes the group commutator [a, b] = (a . b . a^-1 . b^-1) for Alphas.
-    '''
+    """
     raise NotImplementedError
 
 
